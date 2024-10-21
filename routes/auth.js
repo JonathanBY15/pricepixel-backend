d5d300bcb21628bf852df2a78264f2d555d18a14
JonathanBY15 <bariscanyaprak@gmail.com>
2024-10-21 14:00:25 -0400

    refactor

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,3 @@
-// const express = require('express');
-// const { signup, login } = require('../controllers/authController');
-// const { validateSignup, validateLogin } = require('../utils/validators');
-
-// const router = express.Router();
-
-// // Signup Route
-// router.post('/signup', validateSignup, signup);
-
-// // Login Route
-// router.post('/login', validateLogin, login);
-
-// module.exports = router;
-
-
-
 // authRoutes.js
 
 const express = require('express');
