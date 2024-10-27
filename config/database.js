9878a8fa5c2bd9164a217ce60ad39bfe50f463a8
JonathanBY15 <bariscanyaprak@gmail.com>
2024-10-26 21:48:09 -0400

    Deployed DB to Render

--- config/database.js
+++ config/database.js
@@ -1,8 +1,23 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('price_pixel', 'postgres', 'walmart48', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // This line is sometimes required for compatibility with some providers
+      },
+    },
+    logging: false, // optional: set to true to see SQL queries in the console
+  }
+);
 
 module.exports = sequelize;
+
